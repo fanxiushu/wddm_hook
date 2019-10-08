70a4d9937b9fa08c2347898c7356d3847a455565
Fanxiushu <fanxiushu@163.com>
2019-10-08 19:54:58 +0800

    xfsredir files system uploaded

---
+++ filter/ioctl.h
@@ -0,0 +1,11 @@
+/// by fanxiushu 2018-08-31
+
+#pragma once
+
+// {99B9E828-FB01-4ABB-A571-5F2555BBEC0C}
+DEFINE_GUID(GUID_WDDM_FILTER_USER_INTERFACE ,
+	0x99b9e828, 0xfb01, 0x4abb, 0xa5, 0x71, 0x5f, 0x25, 0x55, 0xbb, 0xec, 0xc);
+
+
+/////
+
